b09b76aaa1c181b950637d3646f9ac4cfb9883e0
Omer <Omer@Omer-Bilgisayar>
2017-05-06 00:46:38 +0300

    web

---
+++ Otopark/src/deneme/Hosgeldin.java
@@ -0,0 +1,17 @@
+package deneme;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Hosgeldin {
+
+	String veri="hosgeþdin";
+	
+	public void setVeri(String veri) {
+		this.veri = veri;
+	}
+	
+	public String getVeri() {
+		return veri;
+	}
+}
